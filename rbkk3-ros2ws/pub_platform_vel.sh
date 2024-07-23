9f4f4d758fdf6174e63bbd3355677bea936b7f38
guillermo <guille.rolle@hotmail.com>
2024-07-23 11:15:11 +0200

    Velocity set to show centrifugal forces

--- rbkk3-ros2ws/pub_platform_vel.sh
+++ rbkk3-ros2ws/pub_platform_vel.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-ros2 topic pub /rbkk3/platform_velocity_controller/cmd_vel_unstamped geometry_msgs/msg/Twist "{
+ros2 topic pub /rbkk3/rbkk3_platform_velocity_controller/cmd_vel_unstamped geometry_msgs/msg/Twist "{
     linear: {
         x: 0, 
         y: 0
